cf2a423bdc3695f3139b7fac8abe354accb6ce26
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2018-05-02 11:41:30 +0700

    setup react motion, started work on home animations

--- src/Home.js
+++ src/Home.js
@@ -5,25 +5,25 @@ import { selectHeader } from './actions/index'
 import { bindActionCreators } from 'redux'
 //import video from '../public/typing.mov'
 import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
-
-
+import {Motion, spring} from 'react-motion'
 
 class Home extends Component {
   render() {
     return (
       <section className="container flex" id='landing'>
+        <Motion defaultStyle={{x: 0}} style={{x: spring(20)}}>
+          {value => <div className="landing-content container" style={{paddingLeft: value.x}}>
 
-        <div className="landing-content container">
+            <div className="text-center center-content">
+              <h5 className="font-italic">I'm here to help clients with their business by fixing and creating projects of value</h5>
+              <h1> {'Blah blah blah blahb blah'.toUpperCase()}</h1>
+              <Link className="homepage-button" to="about" spy={true} smooth={true} duration={500} onClick={()=>{this.props.selectHeader('about')}} >
+                <div>About my work</div>
+              </Link>
+            </div>
+          </div>}
+        </Motion>
 
-          <div className="text-center center-content">
-            <h5 className="font-italic">I'm here to help clients with their business by fixing and creating projects of value</h5>
-            <h1> {'Lets build something amazing together'.toUpperCase()}</h1>
-            <Link className="homepage-button" to="about" spy={true} smooth={true} duration={500} onClick={()=>{this.props.selectHeader('about')}} >
-              <div>About my work</div>
-            </Link>
-          </div>
-
-        </div>
       </section>
 
     );
