98bc049943e88320ff789111d6468d106f5b9c3c
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2018-05-03 14:25:25 +0700

    finished homepage animation, setup portfolio

--- src/Home.js
+++ src/Home.js
@@ -8,21 +8,51 @@ import { Link, DirectLink, Element , Events, animateScroll as scroll, scrollSpy,
 import {Motion, spring} from 'react-motion'
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      aboutButton: <div className="intro invisible">
+          <Link className="homepage-button" to="about" spy={true} smooth={true} duration={500} onClick={()=>{this.props.selectHeader('about')}} >
+            <div>About my work</div>
+          </Link>
+        </div>,
+    }
+    this.handleAboutButton = this.handleAboutButton.bind(this)
+  }
+
+  handleAboutButton(){
+    setTimeout(() => {
+      this.setState({ aboutButton: <Motion defaultStyle={{x: -25, o: 0}} style={{x: spring(0, {stiffness: 120,damping: 30}), o: spring(1, {stiffness: 30})}}>
+        {value => <div className="intro" style={{left: value.x, opacity: value.o}}>
+            <Link className="homepage-button" to="about" spy={true} smooth={true} duration={500} onClick={()=>{this.props.selectHeader('about')}} >
+              <div>About my work</div>
+            </Link>
+          </div>}
+      </Motion> })
+    },650)
+
+  }
+
   render() {
+
     return (
       <section className="container flex" id='landing'>
-        <Motion defaultStyle={{x: -25, o: 0}} style={{x: spring(0, {stiffness: 120,damping: 30}), o: spring(1, {stiffness: 30})}}>
-          {value => <div className="landing-content container" style={{left: value.x, opacity: value.o}}>
 
-            <div className="text-center center-content">
-              <h5 className="font-italic">I'm here to help clients with their business by fixing and creating projects of value</h5>
-              <h1> {'Blah blah blah blahb blah'.toUpperCase()}</h1>
-              <Link className="homepage-button" to="about" spy={true} smooth={true} duration={500} onClick={()=>{this.props.selectHeader('about')}} >
-                <div>About my work</div>
-              </Link>
-            </div>
-          </div>}
-        </Motion>
+        <div className="landing-content container" >
+
+          <div className="text-center center-content">
+            <Motion defaultStyle={{x: -25, o: 0}} style={{x: spring(0, {stiffness: 120,damping: 30}), o: spring(1, {stiffness: 30})}}>
+              {value => <div className="intro" style={{left: value.x, opacity: value.o}}>
+                  <h5 className="font-italic">I'm here to help clients with their business by fixing and creating projects of value</h5>
+                  <h1> {'Blah blah blah blahb blah'.toUpperCase()}</h1>
+                </div>}
+            </Motion>
+            {this.handleAboutButton()}
+            {this.state.aboutButton}
+
+
+          </div>
+        </div>
 
       </section>
 
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -19,49 +19,36 @@ class Portfolio extends Component {
     return (
       <section className="text-center" id='portfolio'>
         <h1>Portfolio</h1>
-        <div className="container cards">
-          <div className="row">
-            <div className='card-flip col-lg nopadding'>
-              <div className='front'>
-
-                <span className='name'>bellaWatt</span>
-                <br/>
-                <span className='job'> - front-end web developer - </span>
-              </div>
-              <div className='back'>
-                <span>Skills:</span>
-                <p>HTML, CSS, Javascript. UX/UI designer</p>
-                <a href="http://www.bellawatt.com" target="_blank"><button>Go to site</button></a>
-              </div>
-            </div>
-            <div className='card-flip col-lg nopadding'>
-              <div className='front'>
-
-                <span className='name'>Grey Wolf</span>
-
-                <span className='job'> - front-end web developer - </span>
-              </div>
-              <div className='back'>
-                <span>Skills:</span>
-                <p>HTML, CSS, Javascript. UX/UI designer</p>
+        <div className="container projects">
+          <div className="row project">
+            <div className='col-sm-4'>
+              <div className="card">
 
               </div>
             </div>
-            <div className='card-flip col-lg nopadding col-md justify-content-center'>
-              <div className='front'>
 
-                <span className='name'>Utility Radar</span>
-                <br/>
-                <span className='job'> - front-end web developer - </span>
-              </div>
-              <div className='back'>
-                <span>Skills:</span>
-                <p>HTML, CSS, Javascript. UX/UI designer</p>
-                <a href="http://www.utilityradar.com" target="_blank"><button>Go to site</button></a>
-              </div>
+            <div className="col-sm-8 text-left">
+              <h5>bellaWatt</h5>
             </div>
           </div>
 
+          <div className="row project">
+            <div className="card">
+
+            </div>
+            <div className="col-8 text-left">
+              <h5>AOP</h5>
+            </div>
+          </div>
+
+          <div className="row project">
+            <div className="card">
+
+            </div>
+            <div className="col-8 text-left">
+              <h5>Utility Radar</h5>
+            </div>
+          </div>
         </div>
 
       </section>
