6f2df596ab0093726e2e8ba0d6125229e9ac9132
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2018-05-04 11:51:06 +0700

    finished animation on portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,18 +1,90 @@
 import React, { Component } from 'react'
-
+import Waypoint from 'react-waypoint'
+import {Motion, spring} from 'react-motion'
 
 class Portfolio extends Component {
   constructor(props){
     super(props)
     this.state = {
-      front: true,
+      bellawatt: <div className="row project invisible">
+        <div className="card">
+          <img src='bellawatt.png'/>
+        </div>
+        <div className="col-sm-8 text-left project-info">
+          <h4>bellaWatt</h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>,
+      aop: <div className="row project invisible">
+        <div className="card">
+          <img className='aop' src='nnsa.png'/>
+        </div>
+        <div className="col-8 text-left project-info">
+          <h4>AOP</h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>,
+      utilityRadar: <div className="row project invisible">
+        <div className="card">
+          <img className='utility-radar' src='apple-icon.png'/>
+        </div>
+        <div className="col-8 text-left project-info">
+          <h4>Utility Radar</h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>
     }
-
-    this.handleCard = this.handleCard.bind(this)
+    this.handleBellawatt = this.handleBellawatt.bind(this)
+    this.handleAOP = this.handleAOP.bind(this)
+    this.handleUtilityRadar = this.handleUtilityRadar.bind(this)
   }
 
-  handleCard(event){
-    this.setState({front: !this.state.front})
+  handleBellawatt(){
+    this.setState({ bellawatt: <Motion defaultStyle={{x: 25, o: 0}} style={{x: spring(0, {stiffness: 120 }), o: spring(1)}}>
+      {value => <div className="row project" style={{top: value.x, opacity: value.o}}>
+        <div className="card">
+          <img src='bellawatt.png'/>
+        </div>
+        <div className="col-sm-8 text-left project-info">
+          <h4><a href='http://www.bellawatt.com' target="_blank">bellaWatt</a></h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>}
+    </Motion>})
+  }
+
+  handleAOP(){
+    this.setState({ aop: <Motion defaultStyle={{x: 25, o: 0}} style={{x: spring(0, {stiffness: 120 }), o: spring(1)}}>
+      {value => <div className="row project" style={{top: value.x, opacity: value.o}}>
+        <div className="card">
+          <img className='aop' src='nnsa.png'/>
+        </div>
+        <div className="col-8 text-left project-info">
+          <h4>AOP [Redacted]</h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>}
+    </Motion>})
+  }
+
+  handleUtilityRadar(){
+    this.setState({ utilityRadar: <Motion defaultStyle={{x: 25, o: 0}} style={{x: spring(0, {stiffness: 120 }), o: spring(1)}}>
+      {value => <div className="row project" style={{top: value.x, opacity: value.o}}>
+        <div className="card">
+          <img className='utility-radar' src='apple-icon.png'/>
+        </div>
+        <div className="col-8 text-left project-info">
+          <h4><a href='http://www.utilityradar.com' target="_blank">Utility Radar</a></h4>
+          <h5>Front-end Developer</h5>
+          <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
+        </div>
+      </div>}
+    </Motion>})
   }
 
   render() {
@@ -20,35 +92,41 @@ class Portfolio extends Component {
       <section className="text-center" id='portfolio'>
         <h1>Portfolio</h1>
         <div className="container projects">
-          <div className="row project">
-            <div className='col-sm-4'>
-              <div className="card">
+          <Waypoint onEnter={()=>{this.handleBellawatt()}}
+            bottomOffset='150px'
+          />
+          {this.state.bellawatt}
 
-              </div>
-            </div>
+          <Waypoint onEnter={()=>{this.handleAOP()}}
+            bottomOffset='150px'
+          />
+          {this.state.aop}
 
-            <div className="col-sm-8 text-left">
-              <h5>bellaWatt</h5>
-            </div>
-          </div>
-
-          <div className="row project">
+          <Waypoint onEnter={()=>{this.handleUtilityRadar()}}
+            bottomOffset='150px'
+          />
+          {this.state.utilityRadar}
+          {/* <div className="row project">
             <div className="card">
-
+              <img className='aop' src='nnsa.png'/>
             </div>
-            <div className="col-8 text-left">
-              <h5>AOP</h5>
+            <div className="col-8 text-left project-info">
+              <h4>AOP</h4>
+              <h5>Front-end Developer</h5>
+              <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
             </div>
-          </div>
+          </div> */}
 
-          <div className="row project">
+          {/* <div className="row project">
             <div className="card">
-
+              <img className='utility-radar' src='apple-icon.png'/>
             </div>
-            <div className="col-8 text-left">
-              <h5>Utility Radar</h5>
+            <div className="col-8 text-left project-info">
+              <h4>Utility Radar</h4>
+              <h5>Front-end Developer</h5>
+              <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
             </div>
-          </div>
+          </div> */}
         </div>
 
       </section>
