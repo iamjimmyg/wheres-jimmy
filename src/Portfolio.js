f194a15c8602e755256faad7497cb4c12245c6f2
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2018-05-06 15:43:56 +0700

    added and positioned portfolio icons

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -82,8 +82,16 @@ class Portfolio extends Component {
         </div>
 
         <div className="col-lg-7 text-left project-info">
-          <h4>AOP [Redacted]</h4>
-          <h5>Front-end Developer</h5>
+          <div className='row'>
+            <div>
+              <img className='nnsa-icon' src='nnsa2.png'/>
+            </div>
+            <div className='text-left'>
+              <h4>AOP [Redacted]</h4>
+              <h5>Front-end Developer</h5>
+            </div>
+
+          </div>
           <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
         </div>
       </div>}
@@ -119,8 +127,16 @@ class Portfolio extends Component {
 
         </div>
         <div className="col-lg-7 text-left project-info">
-          <h4><a href='http://www.bellawatt.com' target="_blank">bellaWatt</a></h4>
-          <h5>Front-end Developer</h5>
+          <div className='row'>
+            <div>
+              <img src='bellawatt.png' className="bellawatt-icon"/>
+            </div>
+            <div className='text-left'>
+              <h4><a href='http://www.bellawatt.com' target="_blank">bellaWatt</a></h4>
+              <h5>Front-end Developer</h5>
+            </div>
+
+          </div>
           <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
         </div>
       </div>}
@@ -153,8 +169,17 @@ class Portfolio extends Component {
         </div>
 
         <div className="col-lg-7 text-left project-info">
-          <h4><a href='http://www.utilityradar.com' target="_blank">Utility Radar</a></h4>
-          <h5>Front-end Developer</h5>
+          <div className='row'>
+            <div>
+              <img src='utility-icon.png' className="utility-icon"/>
+            </div>
+            <div className='text-left'>
+              <h4><a href='http://www.utilityradar.com' target="_blank">Utility Radar</a></h4>
+              <h5>Front-end Developer</h5>
+            </div>
+
+          </div>
+
           <p>Donec hendrerit aliquet augue. In vitae sodales sem. In hac habitasse platea dictumst. Sed eu libero nisi. Mauris vitae condimentum metus. Mauris sit amet velit bibendum, consequat enim eu, porttitor neque. Aliquam quis maximus sapien. Curabitur tristique at nisi et ultricies. Mauris pretium sit amet justo non placerat. Maecenas sit amet nunc molestie, tincidunt nisi in, aliquet neque. Ut dignissim leo sit amet orci varius, non laoreet felis lobortis. Etiam efficitur felis et augue vehicula elementum. Ut sed tellus sem.</p>
         </div>
       </div>}
